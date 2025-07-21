bf8f8b3cd592664ca6f97af888ba8132e12fcb2d
XR Lead <gamemakerx5@gmail.com>
2025-07-21 11:24:09 +0300

    Setup

---
+++ Runtime/Core/IOTCommunicationManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+namespace ThreeLines.IOT
+{
+    public class IOTCommunicationManager : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        enum CommunicationMethod
+        {
+            None,
+            Serial,
+            Bluetooth
+        }
+    }
+}
+
